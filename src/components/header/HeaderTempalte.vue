540bd5369887af99b2c3f787891cb851d1a376bb
zhengzhang <zhengzhang@datamesh.com>
2020-08-06 18:40:26 +0800

    修改测试用例

--- src/components/header/HeaderTempalte.vue
+++ src/components/header/HeaderTempalte.vue
@@ -2,14 +2,16 @@
  * @Author: zhangzheng
  * @Date: 2020-08-06 16:37:09
  * @LastEditors: zhangzheng
- * @LastEditTime: 2020-08-06 16:45:14
+ * @LastEditTime: 2020-08-06 18:39:07
  * @Descripttion: 
 -->
 <template lang="">
-  <div class="header"></div>
+  <div class="header">{{ msg }}</div>
 </template>
 <script>
-export default {};
+export default {
+  props: ["msg"]
+};
 </script>
 <style lang="scss" scoped>
 .header {
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,6 +1,13 @@
+/*
+ * @Author: zhangzheng
+ * @Date: 2020-08-06 15:12:32
+ * @LastEditors: zhangzheng
+ * @LastEditTime: 2020-08-06 18:31:15
+ * @Descripttion:
+ */
 import { expect } from "chai";
 import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from "@/components/header/HeaderTempalte.vue";
 
 describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
